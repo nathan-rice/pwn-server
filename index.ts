fb7e49168166ab8a61fbd529c6d567ed53a4f468
nrice <nathan.r@martiancraft.com>
2019-08-11 07:51:10 -0500

    Initial commit

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import WebSocket from "ws";
+import * as WebSocket from "ws";
 import { Pool } from "pg";
 import Cursor from "pg-cursor";
+import _ from "lodash";
 
 const log = console.log;
 
@@ -9,7 +10,12 @@ const server = new WebSocket.Server({ port: 8080 });
 
 const resources = {
   example: {
-    connection: { host: "host", port: 5432, database: "database", max: 2 },
+    connection: {
+      host: "example.host",
+      port: 5432,
+      database: "example.database",
+      max: 2
+    },
     description: "example resource"
   }
 };
@@ -18,9 +24,10 @@ server.on("connection", async ws => {
   ws.connections = {};
   ws.active = {};
   ws.on("message", async message => {
+    log(message);
     const { resource, type, ...parameters } = JSON.parse(message);
     if (type === "execute") {
-      const { statement, resource, batchSize, queryId } = parameters;
+      const { statement, batchSize, queryId } = parameters;
       const connection = ws.connections[resource];
       if (connection) {
         try {
@@ -62,7 +69,9 @@ server.on("connection", async ws => {
             const start = new Date().getTime();
             const results = await connection.query(statement);
             const duration = new Date().getTime() - start;
-            ws.send(JSON.stringify({ queryId, duration, success: true, results }));
+            ws.send(
+              JSON.stringify({ queryId, duration, success: true, results: results.rows })
+            );
           }
         } catch ({ message: error }) {
           const result = JSON.stringify({ queryId, success: false, error });
@@ -70,70 +79,72 @@ server.on("connection", async ws => {
           ws.send(result);
         }
       }
-    } else if (type === "control") {
-      const { action } = parameters;
-      if (action === "abort") {
-        const { queryId } = parameters;
-        if (ws.active[queryId]) {
-          delete ws.active[queryId];
-          ws.send(JSON.stringify({ resource, action, success: true }));
-        } else {
-          ws.send(
-            JSON.stringify({
-              resource,
-              action,
-              success: false,
-              error: `No active query with id ${queryId}`
-            })
-          );
-        }
-      } else if (action === "resources") {
-        ws.send(JSON.stringify(resources.map(r => r.description)));
-      } else if (action === "open") {
-        const { user, password } = parameters;
-        if (ws.connections[resource]) {
-          try {
-            ws.connections[resource].close();
-          } catch ({ message: error }) {
-            const result = JSON.stringify({
-              resource,
-              action,
-              success: false,
-              error
-            });
-            log(result);
-            ws.send(result);
-          }
-        }
+    } else if (type === "abort") {
+      const { queryId } = parameters;
+      if (ws.active[queryId]) {
+        delete ws.active[queryId];
+        ws.send(JSON.stringify({ resource, type, success: true }));
+      } else {
+        ws.send(
+          JSON.stringify({
+            resource,
+            type,
+            success: false,
+            error: `No active query with id ${queryId}`
+          })
+        );
+      }
+    } else if (type === "resources") {
+      ws.send(
+        JSON.stringify({
+          success: true,
+          results: _.map(resources, r => r.description)
+        })
+      );
+    } else if (type === "open") {
+      const { user, password } = parameters;
+      if (ws.connections[resource]) {
         try {
-          ws.connections[resource] = new Pool({
-            ...resources[resource].connection,
-            user,
-            password
-          });
-          ws.send(JSON.stringify({ resource, action, success: true }));
+          ws.connections[resource].close();
         } catch ({ message: error }) {
           const result = JSON.stringify({
             resource,
-            action,
+            type,
             success: false,
             error
           });
-          console.log(result);
+          log(result);
           ws.send(result);
         }
-      } else if (action === "close") {
-        const { resource } = parameters;
-        const connection = ws.connections[resource];
-        if (connection) {
-          try {
-            connection.close();
-            ws.send(JSON.stringify({ resource, action, success: true }));
-          } catch ({ message: error }) {
-            const result = JSON.stringify({ resource, action, error });
-            console.log(result);
-            ws.send(result);
-          }
+      }
+      try {
+        ws.connections[resource] = new Pool({
+          ...resources[resource].connection,
+          user,
+          password
+        });
+        ws.send(JSON.stringify({ resource, type, success: true }));
+      } catch ({ message: error }) {
+        const result = JSON.stringify({
+          resource,
+          type,
+          success: false,
+          error
+        });
+        console.log(result);
+        ws.send(result);
+      }
+    } else if (type === "close") {
+      const { resource } = parameters;
+      const connection = ws.connections[resource];
+      if (connection) {
+        try {
+          connection.close();
+          ws.send(JSON.stringify({ resource, type, success: true }));
+        } catch ({ message: error }) {
+          const result = JSON.stringify({ resource, type, error });
+          console.log(result);
+          ws.send(result);
         }
       }
     }
